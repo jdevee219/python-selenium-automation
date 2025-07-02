753af391d08d155ce694d3928babf5cc5fab0180
jdevee219 <jdevee219@gmail.com>
2025-07-02 17:26:56 -0400

    Updated sleep to wait

--- features/steps/product_details_page_steps.py
+++ features/steps/product_details_page_steps.py
@@ -1,4 +1,5 @@
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 from behave import given, then
 from time import sleep
 
@@ -10,7 +11,7 @@ SELECTED_COLOR = (By.CSS_SELECTOR, "[data-test='@web/VariationComponent'] div")
 @given('Open target product A-94280240 page')
 def open_target(context):
     context.driver.get(f'https://www.target.com/p/men-39-s-hybrid-shorts-6-34-all-in-motion-8482/-/A-94280240?preselect=93321357#lnk=sametab')
-    sleep(8)
+
 
 
 @then('Verify user can click through colors')
@@ -18,6 +19,9 @@ def click_and_verify_colors(context):
     expected_colors = ['Black', 'Blue', 'Dark Blue', 'Jet Black', 'Red', 'Tan']
     actual_colors = []
 
+    context.driver.wait.until(
+        EC.visibility_of_element_located(COLOR_OPTIONS)
+    )
     colors = context.driver.find_elements(*COLOR_OPTIONS)  # [webelement1, webelement2, webelement3]
     print(colors)
 
